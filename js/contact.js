e207dcb42f92df576185928f65a516870b111376
Brendan Lee <Brendan.Lee@us.yazaki.com>
2024-07-24 14:04:09 -0400

    not sure but files were diff

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,6 @@
 let textBox = document.getElementById("textbox");
 let wordCount = document.getElementById("wordcount")
+let button = document.getElementById("submit")
 
 textBox.addEventListener("input", () => {    
     let textLength = textBox.value.length;
